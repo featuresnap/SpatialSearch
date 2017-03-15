914449bbc8dff6ca3c83bb49c7f6c21eaa9ad5bf
Patrick Kelly <patrick.kelly@sherwin.com>
2017-03-14 21:47:02 -0400

    defined location, bounding box and enclose

--- tests/SpatialSearch.Tests/Tests.fs
+++ tests/SpatialSearch.Tests/Tests.fs
@@ -4,7 +4,9 @@ open SpatialSearch
 open NUnit.Framework
 
 [<Test>]
-let ``hello returns 42`` () =
+let ``encloses`` () =
   let result = Library.hello 42
   printfn "%i" result
   Assert.AreEqual(42,result)
+
+
