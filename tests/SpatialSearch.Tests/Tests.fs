25c8811767e483d7e007d15868e021fc65543b71
Patrick Kelly <patrick.kelly@sherwin.com>
2017-03-17 08:58:44 -0400

    wip

--- tests/SpatialSearch.Tests/Tests.fs
+++ tests/SpatialSearch.Tests/Tests.fs
@@ -1,12 +1,21 @@
 module SpatialSearch.Tests
 
+open System
 open SpatialSearch
+open SpatialSearch.Library
 open NUnit.Framework
 
 [<Test>]
-let ``encloses`` () =
-  let result = Library.hello 42
-  printfn "%i" result
-  Assert.AreEqual(42,result)
+let ``location created correctly`` () =
+    let aLocation = location(1,2,3)
+    let expected = {X=1; Y=2; Z=3}
+    Assert.AreEqual(expected, aLocation)
 
-
+[<TestCase(1,2)>]
+[<TestCase(2,1)>]
+let ``inOrder places items in order from lowest to highest`` (value1:int, value2:int) =
+    let highest =  Math.Max(value1, value2)
+    let lowest = Math.Min(value1, value2)
+    let ordered = inOrder(value1, value2)
+    Assert.AreEqual((lowest, highest), ordered)
+    
